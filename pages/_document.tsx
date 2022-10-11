7e52113d11f9d96e93b482b148aa49cc6f1c53f8
Augustas <jakaitisaugustas@gmail.com>
2022-10-11 21:11:40 +0300

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import type { NextPage } from 'next'
+
+const Document: NextPage = () => {
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
+
+export default Document;
