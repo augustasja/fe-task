2cd5167aa1207d01c99340da84d69e0127d85bbd
Augustas <jakaitisaugustas@gmail.com>
2022-10-12 11:10:39 +0300

    form styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ const Document: NextPage = () => {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap" rel="stylesheet"></link>
             </Head>
             <body>
                 <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,17 @@ const Home: NextPage = () => {
       <section className="main-section">
         <div className="container">
           <div className="wrapper">
-            <div className="left">form</div>
+            <div className="left">
+              <div className="form">
+                <div className="form-content">
+                  <h1>Refer friends and get rewards</h1>
+                  <p>Refer your friends to us and earn hotel booking vouchers. We'll give you 1 coin for each friend that installs our extension. Minimum cash-out at 20 coins.</p>
+                </div>
+                <div className="form-footer">
+                  <span>Limits on max rewards apply.</span>
+                </div>
+              </div>
+            </div>
             <div className="right">asf</div>
           </div>
         </div>
