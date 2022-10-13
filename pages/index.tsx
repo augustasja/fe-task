217e54db93eb885e9b1f93700533e6187df6b0e5
Augustas <jakaitisaugustas@gmail.com>
2022-10-13 12:15:42 +0300

    Step section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Form from "../components/Form"
+import Image from "next/image"
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,40 @@ const Home: NextPage = () => {
           <div className="left">
             <Form />
           </div>
-          <div className="right">asf</div>
+          <div className="right">
+            <div className="step-section">
+              <div className="first-step">
+                <div className="step-logo">
+                  <Image src="/assets/invite.svg" alt="invite" width="100%" height="100%" layout="responsive" />
+                </div>
+                <div className="step-description">
+                  <span>STEP 1</span>
+                  <h2>Invite friends</h2>
+                  <p>Refer friends with your unique referral link.</p>
+                </div>
+              </div>
+              <div className="second-step">
+                <div className="step-logo">
+                  <Image src="/assets/collect-coins.svg" alt="invite" width="100%" height="100%" layout="responsive" />
+                </div>
+                <div className="step-description">
+                  <span>STEP 2</span>
+                  <h2>Collect Coins</h2>
+                  <p>Get 1 coin for each friend that installs our extension using your referral link.</p>
+                </div>
+              </div>
+              <div className="third-step">
+                <div className="step-logo">
+                  <Image src="/assets/voucher.svg" alt="invite" width="100%" height="100%" layout="responsive" />
+                </div>
+                <div className="step-description">
+                  <span>STEP 3</span>
+                  <h2>Collect Coins</h2>
+                  <p>Redeem for a $20 hotel booking voucher once you collect 20 coins.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <section>
