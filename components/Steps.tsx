36e6e85c4b9f29778a02d2fdff63f2a808d11e0e
Augustas <jakaitisaugustas@gmail.com>
2022-10-13 13:31:51 +0300

    Steps component, store section

---
+++ components/Steps.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from "next/image"
+
+const Steps = () => {
+    return (
+        <div className="step-section">
+            <div className="first-step">
+                <Image src="/assets/invite.svg" alt="invite" width={144} height={144} layout="fixed" />
+                <div className="step-content">
+                    <span className="step-count">Step 1</span>
+                    <h2 className="step-header">Invite friends</h2>
+                    <p className="step-description">Refer friends with your unique referral link.</p>
+                </div>
+            </div>
+            <div className="second-step">
+                <Image src="/assets/collect-coins.svg" alt="invite" width={144} height={144} layout="fixed" />
+                <div className="step-content">
+                    <span className="step-count">Step 2</span>
+                    <h2 className="step-header">Collect Coins</h2>
+                    <p className="step-description">Get 1 coin for each friend that installs our extension using your referral link.</p>
+                </div>
+            </div>
+            <div className="third-step">
+                <Image src="/assets/voucher.svg" alt="invite" width={144} height={107} layout="fixed" />
+                <div className="step-content">
+                    <span className="step-count">Step 3</span>
+                    <h2 className="step-header">Collect Coins</h2>
+                    <p className="step-description">Redeem for a $20 hotel booking voucher once you collect 20 coins.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Steps
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Form from "../components/Form"
+import Steps from "../components/Steps"
 import Image from "next/image"
 
 const Home: NextPage = () => {
@@ -11,44 +12,19 @@ const Home: NextPage = () => {
             <Form />
           </div>
           <div className="right">
-            <div className="step-section">
-              <div className="first-step">
-                <div className="step-logo">
-                  <Image src="/assets/invite.svg" alt="invite" width={144} height={144} layout="fixed" />
-                </div>
-                <div className="step-content">
-                  <span className="step-count">STEP 1</span>
-                  <h2 className="step-header">Invite friends</h2>
-                  <p className="step-description">Refer friends with your unique referral link.</p>
-                </div>
-              </div>
-              <div className="second-step">
-                <div className="step-logo">
-                  <Image src="/assets/collect-coins.svg" alt="invite" width={144} height={144} layout="fixed" />
-                </div>
-                <div className="step-content">
-                  <span className="step-count">STEP 2</span>
-                  <h2 className="step-header">Collect Coins</h2>
-                  <p className="step-description">Get 1 coin for each friend that installs our extension using your referral link.</p>
-                </div>
-              </div>
-              <div className="third-step">
-                <div className="step-logo">
-                  <Image src="/assets/voucher.svg" alt="invite" width={144} height={107} layout="fixed" />
-                </div>
-                <div className="step-content">
-                  <span className="step-count">STEP 3</span>
-                  <h2 className="step-header">Collect Coins</h2>
-                  <p className="step-description">Redeem for a $20 hotel booking voucher once you collect 20 coins.</p>
-                </div>
-              </div>
-            </div>
+            <Steps />
           </div>
         </div>
       </section>
-      <section>
-        <div className="container">
-          taegfea
+      <section className="store-section">
+        <div className="container wrapper">
+          <a className="store-block" href="">
+            <Image src="/assets/chrome.svg" alt="chrome-store" width="65" height="58" layout="fixed"/>
+            <div className="store-content">
+              <span className="store-title-1">available in the</span>
+              <span className="store-title-2">chrome web store</span>
+            </div>
+          </a>
         </div>
       </section>
     </main>
