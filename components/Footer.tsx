fbb42b0a38373e8be0d45b3b30242226ab130579
Augustas <jakaitisaugustas@gmail.com>
2022-10-14 09:52:35 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,9 @@ const Footer = () => {
                 <div className="footer-info">
                     <div>
                         <Link className="footer-logo" href="/">
-                            <Image src="/assets/logo.svg" alt="ratepunk-logo" width={125} height={32} layout="fixed" />
+                            <a href="">
+                                <Image src="/assets/logo.svg" alt="ratepunk-logo" width={125} height={32} layout="fixed" />
+                            </a>
                         </Link>
                         <p className="footer-description">Ratepunk compares hotel room prices across the major online travel agencies. When you search for a room, Ratepunk extension scans the top booking sites and runs a price comparison, so you can be confident in knowing you’re getting the best deal!</p>
                     </div>
